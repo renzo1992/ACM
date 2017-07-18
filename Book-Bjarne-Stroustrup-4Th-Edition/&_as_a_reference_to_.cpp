58a8907892185726bae487e20064e2a0d9f958e0
renzo27 <renzocode@users.noreply.github.com>
2017-07-17 20:37:19 -0500

    Create &_as_a_reference_to_.cpp

---
+++ Book-Bjarne-Stroustrup-4Th-Edition/&_as_a_reference_to_.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void print()
+{
+	int v[]={1,2,3,4,5,6,8};
+		for(auto& x:v){
+		cout << x << endl;
+		++x;
+	}
+}
+int main()
+{
+	print();
+	return 0;
+}
